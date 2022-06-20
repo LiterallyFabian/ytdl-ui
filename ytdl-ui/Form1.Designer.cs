6a57146510bbb03e4dc3ee5a6c89257d67b1a3f4
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-06-20 13:50:04 +0200

    Initial commit

---
+++ ytdl-ui/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿using System.ComponentModel;
+
+namespace ytdl_ui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputUrl = new System.Windows.Forms.TextBox();
+            this.buttonMp4 = new System.Windows.Forms.Button();
+            this.buttonMp3 = new System.Windows.Forms.Button();
+            this.buttonThumbnail = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputUrl
+            // 
+            this.inputUrl.Location = new System.Drawing.Point(12, 35);
+            this.inputUrl.Name = "inputUrl";
+            this.inputUrl.Size = new System.Drawing.Size(368, 22);
+            this.inputUrl.TabIndex = 0;
+            this.inputUrl.TextChanged += new System.EventHandler(this.inputUrl_TextChanged);
+            // 
+            // buttonMp4
+            // 
+            this.buttonMp4.Location = new System.Drawing.Point(12, 63);
+            this.buttonMp4.Name = "buttonMp4";
+            this.buttonMp4.Size = new System.Drawing.Size(75, 33);
+            this.buttonMp4.TabIndex = 1;
+            this.buttonMp4.Text = ".mp4";
+            this.buttonMp4.UseVisualStyleBackColor = true;
+            this.buttonMp4.Click += new System.EventHandler(this.buttonMp4_Click);
+            // 
+            // buttonMp3
+            // 
+            this.buttonMp3.Location = new System.Drawing.Point(93, 63);
+            this.buttonMp3.Name = "buttonMp3";
+            this.buttonMp3.Size = new System.Drawing.Size(75, 33);
+            this.buttonMp3.TabIndex = 2;
+            this.buttonMp3.Text = ".mp3";
+            this.buttonMp3.UseVisualStyleBackColor = true;
+            this.buttonMp3.Click += new System.EventHandler(this.buttonMp3_Click);
+            // 
+            // buttonThumbnail
+            // 
+            this.buttonThumbnail.Location = new System.Drawing.Point(174, 63);
+            this.buttonThumbnail.Name = "buttonThumbnail";
+            this.buttonThumbnail.Size = new System.Drawing.Size(75, 33);
+            this.buttonThumbnail.TabIndex = 3;
+            this.buttonThumbnail.Text = "thumb";
+            this.buttonThumbnail.UseVisualStyleBackColor = true;
+            this.buttonThumbnail.Click += new System.EventHandler(this.buttonThumbnail_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 121);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonThumbnail);
+            this.Controls.Add(this.buttonMp3);
+            this.Controls.Add(this.buttonMp4);
+            this.Controls.Add(this.inputUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "(✿◡‿◡)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button buttonMp4;
+        private System.Windows.Forms.Button buttonMp3;
+        private System.Windows.Forms.Button buttonThumbnail;
+
+        private System.Windows.Forms.TextBox inputUrl;
+
+        #endregion
+    }
+}
---
+++ ytdl-ui/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mime;
+using System.Windows.Forms;
+
+namespace ytdl_ui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonMp4.Enabled = false;
+            buttonMp3.Enabled = false;
+            buttonThumbnail.Enabled = false;
+        }
+
+        private void buttonMp4_Click(object sender, EventArgs e)
+        {
+            DownloadMedia(false);
+        }
+        
+        private void buttonMp3_Click(object sender, EventArgs e)
+        {
+            DownloadMedia(true);
+        }
+
+        private void DownloadMedia(bool isMp3)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            const string mp3Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            const string mp4Filter = "MP4 files (*.mp4)|*.mp4|All files (*.*)|*.*";
+            saveFileDialog.Filter = isMp3 ? mp3Filter : mp4Filter;
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+            
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string url = inputUrl.Text;
+                string output = saveFileDialog.FileName;
+
+                Process process = new Process();
+                process.StartInfo.FileName = "youtube-dl.exe";
+                string mp3Argument = $"--extract-audio --audio-format mp3 --output \"{output}\" \"{url}\"";
+                string mp4Argument = $" --merge-output-format mp4 --output \"{output}\" \"{url}\"";
+                process.StartInfo.Arguments = isMp3 ? mp3Argument : mp4Argument;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private void inputUrl_TextChanged(object sender, EventArgs e)
+        {
+            // check if the url is a valid youtube url
+            if (inputUrl.Text.Contains("youtube.com") || inputUrl.Text.Contains("youtu.be"))
+            {
+                buttonMp4.Enabled = true;
+                buttonMp3.Enabled = true;
+                buttonThumbnail.Enabled = true;
+            }
+            else
+            {
+                buttonMp4.Enabled = false;
+                buttonMp3.Enabled = false;
+                buttonThumbnail.Enabled = false;
+            }
+        }
+
+        private void buttonThumbnail_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+            
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string url = inputUrl.Text;
+                string output = saveFileDialog.FileName;
+
+                // download to temp file
+                Process downloadProcess = new Process();
+                downloadProcess.StartInfo.FileName = "youtube-dl.exe";
+                string tmpFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                downloadProcess.StartInfo.Arguments = $"--write-thumbnail --skip-download --output \"{tmpFile}\" \"{url}\"";
+                downloadProcess.Start();
+                downloadProcess.WaitForExit();
+                
+                // convert to png                
+                Process convertProcess = new Process();
+                convertProcess.StartInfo.FileName = "ffmpeg.exe";
+                convertProcess.StartInfo.Arguments = $"-i \"{tmpFile}.webp\" -vf scale=1920:-1 -y \"{output}\"";
+                convertProcess.Start();
+                convertProcess.WaitForExit();
+                
+                // delete temp file
+                File.Delete(tmpFile);
+            }
+        }
+    }
+}
